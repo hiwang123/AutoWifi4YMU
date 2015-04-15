8c9174f3bfbc3cd1599924165b15cabca9c82c2b
hiwang123 <hiwang123@gmail.com>
2015-04-16 02:05:30 +0800

    0416
    sometimes cannot recieve broadcast or too slow

--- src/com/example/autowifi4ymu/MainActivity.java
+++ src/com/example/autowifi4ymu/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.autowifi4ymu;
+﻿package com.example.autowifi4ymu;
 
 import java.io.IOException;
 import java.security.KeyStore;
@@ -56,6 +56,7 @@ public class MainActivity extends ActionBarActivity {
 	Button applyButton;
 	EditText idText, pwdText;
 	TextView mTextView;
+	static Context mContext;
 	boolean activate;
 	static final String STATE = "state";
 	public static String idString, pwdString;
@@ -71,6 +72,7 @@ public class MainActivity extends ActionBarActivity {
 		pwdString = prefs.getString("pwdString","");
 		idText.setText(idString);
 		pwdText.setText(pwdString);
+		mContext = getApplicationContext();
 		
 	}
 	
@@ -92,22 +94,17 @@ public class MainActivity extends ActionBarActivity {
 				editor.putString("pwdString", pwdString);
 				editor.commit();
 				Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
-				wifiInitialize();
+				checkConnectedTargetWifi();
 			}
 		});
 		mTextView=(TextView) findViewById(R.id.textView5);
-		mTextView.setText("1.使用區域:陽明大學有wifi之處\n" +
-				"2.學校行政系統更換密碼需重新輸入帳密並按確定" +
-				"3.網路不穩可能導致異常,例如:宿舍\n" +
-				"4.若帳號異常非此app所能處理,請洽詢網管OAQ\n");
-	}
-	
-	public void wifiInitialize(){
-		WifiManager wiFiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
-		if( wiFiManager.isWifiEnabled() ){
-			wiFiManager.setWifiEnabled(false);
-			wiFiManager.setWifiEnabled(true);
-		}
+		mTextView.setText("使用:輸入學校行政系統之帳密,Go!\n"+
+				"區域:陽明大學有wifi之處\n" +
+				"功能:連線至ymu後,自動認證帳密\n" +
+				"1.每次使用wifi時不需重新開啟此app\n"+
+				"2.更改學校行政系統帳密需重新輸入\n"+
+				"3.wifi狀態需為「已連線」,認證完成會顯示'connecting'\n" +
+				"4.若無法自動登入,需重新啟動此app再按Go!\n");
 	}
 
 	@Override
@@ -144,119 +141,92 @@ public class MainActivity extends ActionBarActivity {
 			SharedPreferences prefs = context.getSharedPreferences(STATE, Context.MODE_PRIVATE);
 			idString = prefs.getString("idString","");
 			pwdString = prefs.getString("pwdString","");
-		//	Toast.makeText(context, action, Toast.LENGTH_SHORT).show();
 			this.context = context;
+			Toast.makeText(context, "wifi", Toast.LENGTH_SHORT).show();
 	        if ( WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action) ) {
 	        	NetworkInfo netInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
 	            if (netInfo.isConnected()) {
-	          // 	Toast.makeText(context, "Wifi on", Toast.LENGTH_SHORT).show();
+	            	//Toast.makeText(context, "networkStateChange", Toast.LENGTH_SHORT).show();
 	                checkConnectedTargetWifi();
 	            }
 	        }
 		}
 		
-		
-		public void checkConnectedTargetWifi(){
-			String targetSSID = "ymu";
-			boolean connected = false;
-			
-			WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-			WifiInfo wifi = wifiManager.getConnectionInfo();
-			Log.d("blah", wifi.toString());
-	        if (wifi != null) {
-	            String ssid = wifi.getSSID();
-	            connected = ssid.equals(targetSSID) || ssid.equals("\""+targetSSID+"\"");
-	        }
-	        if(connected){
-	        	Toast.makeText(context, "connecting to "+targetSSID, Toast.LENGTH_SHORT).show();
-	        	AutoWifiAuthentic();
-	        }
-	        else{
-	        	Toast.makeText(context, "oops!", Toast.LENGTH_SHORT).show();
-	        }
-	    }
-		
-		public void AutoWifiAuthentic(){
-			
-			//String result="e";
-			
-			Thread thread = new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					//HttpClient client = getNewHttpClient();
-					HttpClient client = new DefaultHttpClient();
-					HttpParams params = new BasicHttpParams();
-					params.setParameter("http.protocol.handle-redirects",false);
-				//	HttpPost httppost = new HttpPost("https://securelogin.arubanetworks.com/auth/index.html/u");
-					HttpPost httppost = new HttpPost("http://3comwx.ym.edu.tw/aaa/wba_form2.html");
-					httppost.setParams(params);
-					String result="e";
-
-			        try {
-			        	// Add data
-			        	List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-			        	nameValuePairs.add(new BasicNameValuePair("fname", "wba_login"));
-			        	nameValuePairs.add(new BasicNameValuePair("username", idString));
-			            nameValuePairs.add(new BasicNameValuePair("key", pwdString));
-			            
-			      //  	nameValuePairs.add(new BasicNameValuePair("user", idString));
-			      //  	nameValuePairs.add(new BasicNameValuePair("password", pwdString));
-						httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-						
-						// Execute HTTP Post Request
-						HttpResponse response = client.execute(httppost);
-
-				        result = EntityUtils.toString(response.getEntity());
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			        
-			     /*   HttpGet httpGet = new HttpGet("http://www.google.com.tw/");
-			        try {
-						HttpResponse response = client.execute(httpGet);
-						Looper.prepare();  
-						Toast.makeText(context, "Connect Successful", Toast.LENGTH_SHORT).show();
-						Looper.loop();
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						Looper.prepare();  
-		                Toast.makeText(context, "Wrong password", Toast.LENGTH_SHORT).show();  
-		                Looper.loop();
-						e.printStackTrace();
-					} finally {
-					     client.getConnectionManager().shutdown();
-					}  */
-				}
-			});
-			thread.start();
-		}		
-		
-	/*	public HttpClient getNewHttpClient() {
-		    try {
-		        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-		        trustStore.load(null, null);
-
-		        SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
-		        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-
-		        HttpParams params = new BasicHttpParams();
-		        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-		        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
-
-		        SchemeRegistry registry = new SchemeRegistry();
-		        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-		        registry.register(new Scheme("https", sf, 443));
-
-		        ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
-
-		        return new DefaultHttpClient(ccm, params);
-		    } catch (Exception e) {
-		        return new DefaultHttpClient();
-		    }
-		}   */
 	}
+	
+	public static void checkConnectedTargetWifi(){
+		String targetSSID = "ymu";
+		boolean connected = false;
+		
+		WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wifi = wifiManager.getConnectionInfo();
+        if (wifi != null) {
+            String ssid = wifi.getSSID();
+            connected = ssid.equals(targetSSID) || ssid.equals("\""+targetSSID+"\"");
+        }
+        if(connected){
+        	Toast.makeText(mContext, "connecting to "+targetSSID, Toast.LENGTH_SHORT).show();
+        	AutoWifiAuthentic();
+        }
+    }
+	
+	public static void AutoWifiAuthentic(){
+		
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				//HttpClient client = getNewHttpClient();
+				HttpClient client = new DefaultHttpClient();
+				HttpParams params = new BasicHttpParams();
+				params.setParameter("http.protocol.handle-redirects",false);
+				HttpPost httppost1 = new HttpPost("https://securelogin.arubanetworks.com/auth/index.html/u");
+				HttpPost httppost2 = new HttpPost("http://3comwx.ym.edu.tw/aaa/wba_form2.html");
+				httppost1.setParams(params);
+				httppost2.setParams(params);
+				String result1="e", result2="e";
+
+		        try {
+		        	// Add data
+		        	List<NameValuePair> nameValuePairs1 = new ArrayList<NameValuePair>(2);
+		            
+		        	nameValuePairs1.add(new BasicNameValuePair("user", idString));
+		        	nameValuePairs1.add(new BasicNameValuePair("password", pwdString));
+		             
+					httppost1.setEntity(new UrlEncodedFormEntity(nameValuePairs1));
+					
+					// Execute HTTP Post Request
+					HttpResponse response1 = client.execute(httppost1);
+
+			        result1 = EntityUtils.toString(response1.getEntity());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        
+		        try {
+		        	// Add data
+		        	List<NameValuePair> nameValuePairs2 = new ArrayList<NameValuePair>(3);
+		            	        	
+		        	nameValuePairs2.add(new BasicNameValuePair("fname", "wba_login"));
+		        	nameValuePairs2.add(new BasicNameValuePair("username", idString));
+		            nameValuePairs2.add(new BasicNameValuePair("key", pwdString));
+		            
+					httppost2.setEntity(new UrlEncodedFormEntity(nameValuePairs2));
+					
+					// Execute HTTP Post Request
+					HttpResponse response2 = client.execute(httppost2);
+
+			        result2 = EntityUtils.toString(response2.getEntity());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        
+			}
+		});
+		thread.start();
+	}		
 	
 }
